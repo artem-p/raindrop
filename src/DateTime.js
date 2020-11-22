3fd2af09d8ac6f73835a7b7cdd11034ba2b4085b
Artyom Pugachev <artempugachev1@gmail.com>
2020-11-22 21:43:49 +0300

    add effect to update timer

--- src/DateTime.js
+++ src/DateTime.js
@@ -1,8 +1,18 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import './DateTime.css';
 
 function DateTime() {
     const [date, setDate] = useState(new Date());
+
+    useEffect(() => {
+        const timer = setInterval(()=>setDate(new Date()), 1000);
+
+        return function cleanup() {
+            clearInterval(timer);
+        }
+        
+    }, []);
+
 
     return (
         <div className="datetime">
