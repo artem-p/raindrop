4afbcfef7c084d0fd52d742682c3a3a21b758b2e
Artyom Pugachev <artempugachev1@gmail.com>
2020-11-28 16:12:37 +0300

    add api urls

--- src/requests.js
+++ src/requests.js
@@ -3,6 +3,6 @@ const API_KEY = "3a7c9da63552f741f703b56817b3e594";
 const city = "Saint Petersburg, ru";
 
 const requests = {
-    fetchCurrentWeather: `api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}`,
-    fetchForecast: `api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${API_KEY}`
+    fetchCurrentWeather: `weather?q=${city}&appid=${API_KEY}`,
+    fetchForecast: `forecast?q=${city}&appid=${API_KEY}`
 }
