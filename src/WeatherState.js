8ad2cbc74c4d8eb5baa438af36a41545fe16f378
Artyom Pugachev <artempugachev1@gmail.com>
2021-09-08 15:02:49 +0300

    grid template

--- src/WeatherState.js
+++ src/WeatherState.js
@@ -7,7 +7,7 @@ function WeatherState({ weatherCode, weatherText, temp, feelsLike, isDay }) {
     
     return (
         <div className='weather'>
-            <div className="weather__description">
+            {/* <div className="weather__description">
                 <div className="weather__icon">
                     <WeatherIcon className="weather__icon" weatherCode={weatherCode} isDay={isDay}/>
                 </div>
@@ -17,7 +17,16 @@ function WeatherState({ weatherCode, weatherText, temp, feelsLike, isDay }) {
             <div className="temperature">
                 <div className="temperature__measured">{temp ? (temp - 273.15).toFixed(0) : ""} {' '} °С</div>
                 <div className="temperature__feelslike">Feels like {feelsLike ? (feelsLike - 273.15).toFixed(0) : ""}{' '}°С</div>
-            </div>
+            </div> */}
+
+                <div className="weather__icon">
+                    <WeatherIcon className="weather__icon" weatherCode={weatherCode} isDay={isDay}/>
+                </div>
+
+                <div className="temperature__measured">{temp ? (temp - 273.15).toFixed(0) : ""} {' '} °С</div>
+                <div className="weather__text">{weatherText}</div>
+
+                <div className="temperature__feelslike">Feels like {feelsLike ? (feelsLike - 273.15).toFixed(0) : ""}{' '}°С</div>
         </div>
     )
 }
