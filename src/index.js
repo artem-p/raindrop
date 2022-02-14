302acf0804f9baee4920a6d1eb27eeff69dd6c58
Artyom Pugachev <artempugachev1@gmail.com>
2022-02-14 22:52:05 +0300

    add routes

--- src/index.js
+++ src/index.js
@@ -1,12 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './index.css';
 import App from './App';
+import Dashboard from './dashboard/Dashboard';
+import Map from './map/Map';
 
 ReactDOM.render(
   <React.StrictMode>
     <BrowserRouter>
+      <Routes>
+      <Route path="/" element={<App />} />
+        <Route path="dashboard" element={<Dashboard />} />
+        <Route path="map" element={<Map />} />
+      </Routes>
       <App />
     </BrowserRouter>
   </React.StrictMode>,
