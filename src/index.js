4b8aa0deb74c8d2f53573287a5dcf473587dad54
Artyom Pugachev <artempugachev1@gmail.com>
2022-02-18 17:45:41 +0300

    default route

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import './index.css';
 import App from './App';
 import Dashboard from './dashboard/Dashboard';
@@ -10,6 +10,8 @@ ReactDOM.render(
   <React.StrictMode>
     <BrowserRouter>
       <Routes>
+        <Route exact path='/raindrop' element={<Navigate to="/raindrop/dashboard" />} />
+          
         <Route path="/raindrop" element={<App />}>
           <Route path="dashboard" element={<Dashboard />} />
           <Route path="map" element={<Map />} />
