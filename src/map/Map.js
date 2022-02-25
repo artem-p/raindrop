949116a1564db4bd9f71f853843a01f3aeb2336e
Artyom Pugachev <artempugachev1@gmail.com>
2022-02-25 16:53:56 +0300

    add precipitation layer

--- src/map/Map.js
+++ src/map/Map.js
@@ -2,7 +2,8 @@ import React, { useRef, useEffect, useState } from 'react';
 import 'mapbox-gl/dist/mapbox-gl.css';
 import mapboxgl from '!mapbox-gl'; // eslint-disable-line import/no-webpack-loader-syntax
 import './Map.css';
-
+import { OWM_API_KEY } from '../requests';
+console.log(OWM_API_KEY);
 
 mapboxgl.accessToken = 'pk.eyJ1IjoiYXJ0ZW1wdWdhY2hldiIsImEiOiJja3l1NWJwZ3owM2IxMnd0NDUydDZqazUyIn0.b1G2XUsJEhO49s7vTH2q1Q';
 
@@ -11,15 +12,36 @@ function Map() {
   const map = useRef(null);
   const [long, setLong] = useState(30);
   const [lat, setLat] = useState(59.9);
-  const [zoom, setZoom] = useState(9);
+  const [zoom, setZoom] = useState(6);
 
   useEffect(() => {
     if (map.current) return; // initialize map only once
-      map.current = new mapboxgl.Map({
+    
+    map.current = new mapboxgl.Map({
       container: mapContainer.current,
-      style: 'mapbox://styles/mapbox/satellite-streets-v11',
+      style: 'mapbox://styles/mapbox/streets-v11',
       center: [long, lat],
       zoom: zoom
+    });
+
+    map.current.on('load', function(){
+      map.current.addLayer({
+        "id": "precipitation",
+        "type": "raster",
+        "source": {
+          "type": "raster",
+          "tiles": ["https://tile.openweathermap.org/map/precipitation_new/{z}/{x}/{y}.png?appid=" + OWM_API_KEY],
+          "tileSize": 256
+        },
+        "minzoom": 0,
+        "maxzoom": 22
+      });
+
+      map.current.setPaintProperty(
+        'precipitation',
+        'raster-opacity',
+        1
+      )
     });
   });
 
