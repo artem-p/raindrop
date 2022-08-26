84396e17a37416b598bd4756612ce85a2ba6a9fe
Artyom Pugachev <artempugachev1@gmail.com>
2022-08-26 16:32:38 +0300

    forecast todo

---
+++ src/dashboard/Forecast.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import './Forecast.css'
+
+
+function Forecast() {
+    //  todo
+    //  2 вкладки - обзор и график
+    //  обзор как на фореке по дням. Значок и температура мин макс
+  return (
+    <div>Forecast</div>
+  )
+}
+
+export default Forecast
