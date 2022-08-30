d144fca48e764f8af25c5792ce969674ea2c2750
Artyom Pugachev <artempugachev1@gmail.com>
2022-08-30 18:12:17 +0300

    yr api

--- src/api.js
+++ src/api.js
@@ -6,14 +6,22 @@ const owmApi = axios.create({
     baseURL: "https://api.openweathermap.org/data/2.5",
 });
 
-const metNoApi = axios.create({
-    baseURL: 'https://api.met.no/weatherapi',
-})
-
 const owmRequests = {
     fetchCurrentWeather: (lat, lon) => owmApi.get(`weather?lat=${lat}&lon=${lon}&appid=${OWM_API_KEY}`),
     fetchForecast: (lat, lon) => {return `forecast?lat=${lat}&lon=${lon}&appid=${OWM_API_KEY}`},
     fetchDailyForecast: (lat, lon) => {return `onecall?lat=${lat}&lon=${lon}&exclude=current,hourly,alerts&appid=${OWM_API_KEY}`},
+}
+
+const metNoApi = axios.create({
+    baseURL: 'https://api.met.no/weatherapi',
+})
+
+const yrApi = axios.create({
+    baseURL: 'https://www.yr.no/api/v0/'
+})
+
+const yrRequests = {
+    fetchForecast: (lat, lon) => yrApi.get(`locations/${lat},${lon}/forecast`)
 }
 
 
--- src/dashboard/Forecast.js
+++ src/dashboard/Forecast.js
@@ -10,9 +10,8 @@ function Forecast({lat, lon}) {
   //  обзор как на фореке по дням. Значок и температура мин макс
   //  дни со значками в виде вкладок, по вкладке переход на день подробно?
 
-  //  todo met.no пропуски в прогнозе
-  //  сравнить met.no и owm для одинаковых координат
-  //  по дням брать с owm?
+  //  использовать v0 api https://www.yr.no/api/v0/locations/60.300,30.600/forecast
+  //  локация https://www.yr.no/api/v0/locations/suggest?language=en&q=%D0%B2%D0%BE%D0%B5%D0%B9%D0%BA%D0%BE%D0%B2%D0%BE
   
   return (
       <Card className='forecast-card'>
