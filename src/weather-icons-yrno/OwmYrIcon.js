84dd2789c847a103c63569059c17f122406f9bb1
Artyom Pugachev <artempugachev1@gmail.com>
2023-01-12 15:13:59 +0300

    yr owm component

---
+++ src/weather-icons-yrno/OwmYrIcon.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+
+function OwmYrIcon({owmCode}) {
+    // return yr icon by owm weather code
+    
+
+    return (
+        <div>OwmYrIcon</div>
+    )
+}
+
+export default OwmYrIcon
