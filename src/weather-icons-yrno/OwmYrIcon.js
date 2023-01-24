0d12a7c497c1fb6a83b97a967b26fa18ee81b790
Artyom Pugachev <artempugachev1@gmail.com>
2023-01-24 18:03:49 +0300

    links

--- src/weather-icons-yrno/OwmYrIcon.js
+++ src/weather-icons-yrno/OwmYrIcon.js
@@ -4,6 +4,10 @@ import YrIcon from './YrIcon'
 
 function OwmYrIcon({owmCode}) {
     // return yr icon by owm weather code
+    
+    // owm codes https://openweathermap.org/weather-conditions
+    // yr codes https://api.met.no/weatherapi/weathericon/2.0/documentation
+
     let yrCode = '';
 
     switch(owmCode) {
