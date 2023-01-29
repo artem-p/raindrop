722d3b7a342750eea2dd540a889ba89ebaf10b66
Artyom Pugachev <artempugachev1@gmail.com>
2023-01-29 23:19:20 +0300

    todo

--- src/weather-icons-yrno/OwmYrIcon.js
+++ src/weather-icons-yrno/OwmYrIcon.js
@@ -1,7 +1,9 @@
 import React from 'react'
 import YrIcon from './YrIcon'
 
-// todo 
+// todo generate icon imports https://github.com/smartprocure/directory-metagen
+
+// mock require context - better don't use it at all 
 // https://stackoverflow.com/questions/38332094/how-can-i-mock-webpacks-require-context-in-jest
 // https://dev.to/ansonh/simplest-way-to-install-babel-plugins-in-create-react-app-7i5
 
