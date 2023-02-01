bb2030d7565bb20684fc575df9e00403c0616ce9
Artyom Pugachev <artempugachev1@gmail.com>
2023-02-01 22:57:54 +0300

    react test

--- src/weather-icons-yrno/OwmYrIcon.js
+++ src/weather-icons-yrno/OwmYrIcon.js
@@ -1,11 +1,6 @@
 import React from 'react'
 import YrIcon from './YrIcon'
 
-// todo generate icon imports https://github.com/smartprocure/directory-metagen
-
-// mock require context - better don't use it at all 
-// https://stackoverflow.com/questions/38332094/how-can-i-mock-webpacks-require-context-in-jest
-// https://dev.to/ansonh/simplest-way-to-install-babel-plugins-in-create-react-app-7i5
 
 const TestSum = (first, second) => {
     return first + second
@@ -35,7 +30,9 @@ function OwmYrIcon({owmCode}) {
     }
 
     return (
-        <YrIcon weatherSymbol={yrCode}/>
+        <div>
+            {yrCode && <YrIcon weatherSymbol={yrCode}/>}
+        </div>
     )
 }
 
--- src/weather-icons-yrno/tests/OwmYrIcon.spec.js
+++ src/weather-icons-yrno/tests/OwmYrIcon.spec.js
@@ -1,4 +1,35 @@
-import { TestSum } from "../OwmYrIcon"
+import { TestSum, OwmYrIcon } from "../OwmYrIcon"
+
+import React from "react";
+import { render, screen } from '@testing-library/react';
+
+//  https://www.digitalocean.com/community/tutorials/how-to-test-a-react-app-with-jest-and-react-testing-library
+//  https://robertmarshall.dev/blog/test-images-in-react-with-jest/
+
+
+// let container = null
+
+// beforeEach(() => {
+//     // setup a DOM element as a render target
+//     container = document.createElement("div");
+//     document.body.appendChild(container);
+// });
+
+
+// afterEach(() => {
+//     // cleanup on exiting
+//     unmountComponentAtNode(container);
+//     container.remove();
+//     container = null;
+// });
+
+
+it("renders icon", () => {
+    render(<OwmYrIcon owmCode={200}/> );
+    const icon = screen.getByRole("img")
+    console.log(icon)
+});
+
 
 it('sums numbers', () => {
     expect(TestSum(1, 2)).toEqual(3)
