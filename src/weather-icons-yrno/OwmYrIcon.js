50bd3e19680aec8894eb854f28186f6e21165151
Artyom Pugachev <artempugachev1@gmail.com>
2023-02-14 22:43:41 +0300

    use icon in cloudiness

--- src/weather-icons-yrno/OwmYrIcon.js
+++ src/weather-icons-yrno/OwmYrIcon.js
@@ -3,40 +3,41 @@ import YrIcon from './YrIcon'
 
 
 function OwmYrIcon({owmCode, isDay, cloudiness}) {
-    //  todo clouds and no clouds icons
-    
+    const CLOUDINESS_LIMIT = 80
+    // return yr icon by owm weather code
+
     //  'showers' icons - sun. without showers - cloudy.
     //  clearksy, fair, cloudy, partly cloudy
 
-    // return yr icon by owm weather code
-    
     // owm codes https://openweathermap.org/weather-conditions
     // yr codes https://api.met.no/weatherapi/weathericon/2.0/documentation
-
     let yrCode = '';
 
     const owmYrDict = {
         // owmCode: ['cloud (neutral) Icon', 'clear icon']
-
         200: ['lightrainandthunder', 'lightrainshowersandthunder'],
         201: ['rainandthunder', 'rainshowersandthunder'],
         202: ['heavyrainandthunder', 'heavysnowshowersandthunder' ],
         210: ['lightrainandthunder', 'lightrainshowersandthunder'],
         211: ['rainandthunder', 'rainshowersandthunder'],
         212: ['heavyrainandthunder', 'heavyrainshowersandthunder'],
-        221: ['heavyrainandthunder', 'heavyrainshowersandthunder'],     //  ? ragged thunderstorm
+        221: ['rainandthunder', 'rainshowersandthunder'],               //  ? ragged thunderstorm
         230: ['lightrainandthunder', 'lightrainshowersandthunder'],     //  thunderstorm with light drizzle
         231: ['lightrainandthunder', 'lightrainshowersandthunder'],     //  thunderstorm with drizzle
         232: ['lightrainandthunder', 'lightrainshowersandthunder'],     //  thunderstorm with heavy drizzle
     }
 
-    //  todo cloudiness < 80 use first icon
-    //  > =80 use second icon and add day/night
-
     if (owmCode in owmYrDict) {
         const yrCodes = owmYrDict[owmCode]
-
-        yrCode = isDay ? yrCodes[0] : yrCodes[1]
+        
+        if (cloudiness > CLOUDINESS_LIMIT) {
+            // use neutral icon without day/night variants
+            yrCode = yrCodes[0]
+        } else {
+            // use day/night icons
+            const iconName = yrCodes[1]
+            yrCode = iconName + (isDay ? '_day' : '_night')
+        }
     }
 
     return (
