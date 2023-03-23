42cb18a721d970afd1234cc0d735aceabaebe546
Artyom Pugachev <artempugachev1@gmail.com>
2023-03-23 18:08:23 +0300

    move fetch forecast

--- src/dashboard/Dashboard.js
+++ src/dashboard/Dashboard.js
@@ -1,7 +1,10 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
 import './Dashboard.css'
 import CurrentWeather from './CurrentWeather'
 import Forecast from './forecast/Forecast'
+import DailyForecast from './forecast/daily/DailyForecast'
+
+import { yrRequests } from '../api'
 
 
 function Dashboard() {
@@ -9,11 +12,29 @@ function Dashboard() {
   const lat= Math.random() * 180 - 90
   const lon = Math.random() * 360 - 180
 
+  // todo fetch forecast here
+
+  const [forecast, setForecast] = useState({})
+
+  async function fetchForecast(lat, lon) {
+    const request = await yrRequests.fetchForecast(lat, lon);
+    setForecast(request.data);
+  }
+
+  useEffect(() => {
+    fetchForecast(lat, lon);
+
+    const requestInterval = setInterval(fetchForecast, 60000 * 10, lat, lon);
+
+    return () => clearInterval(requestInterval);
+  }, [lat, lon]); 
+
+
   return (
     <div className="dashboard">
       <CurrentWeather lat={lat} lon={lon} />
-
-      <Forecast lat={lat} lon={lon} />
+      {/* <DailyForecast forecast={forecast} /> */}
+      <Forecast forecast={forecast} />
     </div>
   )
 }
--- src/dashboard/forecast/Forecast.js
+++ src/dashboard/forecast/Forecast.js
@@ -2,9 +2,6 @@ import React, { useState, useEffect } from 'react'
 import Card from 'react-bootstrap/Card'
 import DailyForecast from './daily/DailyForecast'
 
-import { yrRequests } from '../../api'
-
-
 import './Forecast.css'
 
 
@@ -17,20 +14,8 @@ function Forecast({lat, lon}) {
 
   //  использовать v0 api https://www.yr.no/api/v0/locations/60.300,30.600/forecast
   //  локация https://www.yr.no/api/v0/locations/suggest?language=en&q=%D0%B2%D0%BE%D0%B5%D0%B9%D0%BA%D0%BE%D0%B2%D0%BE
-  const [forecast, setForecast] = useState({});
 
-  async function fetchForecast(lat, lon) {
-    const request = await yrRequests.fetchForecast(lat, lon);
-    setForecast(request.data);
-  }
-
-  useEffect(() => {
-    fetchForecast(lat, lon);
-
-    const requestInterval = setInterval(fetchForecast, 60000 * 10, lat, lon);
-
-    return () => clearInterval(requestInterval);
-  }, [lat, lon]);
+  
 
   
   return (
