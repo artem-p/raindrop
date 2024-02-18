60c04a2a7b2467b77569dfecbe53460889708f05
Artyom Pugachev <artempugachev1@gmail.com>
2024-02-18 23:29:19 +0300

    plot temp

--- data/gfs.py
+++ data/gfs.py
@@ -1,12 +1,16 @@
+import os
+
 import cfgrib
 import xarray as xr
 import matplotlib.pyplot as plt
 
+
 #   https://docs.xarray.dev/en/stable/examples/ERA5-GRIB-example.html
 #   https://stackoverflow.com/questions/67963199/xarray-from-grib-file-to-dataset
 
-ds = xr.load_dataset("gfs.f000.grb2", engine="cfgrib")
+file_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'gfs.f000.grb2')
+ds = xr.load_dataset(file_path, engine="cfgrib")
 
 ds = ds - 273.15
-ds.t[0].plot(cmap=plt.cm.coolwarm)
-ds.t[0].show()
+plot = ds.t.plot(cmap=plt.cm.coolwarm)
+plt.show()
