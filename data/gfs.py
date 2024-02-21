776614c42db492207f8ba84aadceb5e7cec64f23
Artyom Pugachev <artempugachev1@gmail.com>
2024-02-21 22:20:37 +0300

    coastlines

--- data/gfs.py
+++ data/gfs.py
@@ -3,6 +3,8 @@ import os
 import cfgrib
 import xarray as xr
 import matplotlib.pyplot as plt
+import cartopy.crs as ccrs
+import cartopy
 
 
 #   https://docs.xarray.dev/en/stable/examples/ERA5-GRIB-example.html
@@ -12,5 +14,9 @@ file_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'gfs.f000.g
 ds = xr.load_dataset(file_path, engine="cfgrib")
 
 ds = ds - 273.15
-plot = ds.t.plot(cmap=plt.cm.coolwarm)
+
+fig = plt.figure(figsize=(10, 10))
+axes = plt.axes(projection=ccrs.Robinson())
+axes.coastlines(resolution='10m')
+plot = ds.t.plot(cmap=plt.cm.coolwarm, transform=ccrs.PlateCarree(), cbar_kwargs={"shrink": 0.6})
 plt.show()
